5e4f918a25e493b521a5069480d5638748512fea
Andoni Sánchez <asanchez156@ikaslke.ehu.es>
2015-07-08 20:07:10 +0200

    Añadidas pregunta y respuesta, it works

--- app.js
+++ app.js
@@ -12,10 +12,11 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+//app.set('controllers', path.join(__dirname, 'controllers'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
