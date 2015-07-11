7381bd1de664b5fdb3e0d27355bc53caafc9a926
Andoni Sánchez <asanchez156@ikasle.ehu.es>
2015-07-11 13:44:48 +0200

    Añadidos creditos

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,9 @@ router.get('/', function(req, res) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+/* GET author page. */
+router.get('/', function(req, res) {
+  res.render('author', { nombre: 'Andoni' });
+});
+
 module.exports = router;
