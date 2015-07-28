2d6e2cce1525d05eda27f784197d88f99efe66da
Andoni Sánchez <asanchez156@ikasle.ehu.es>
2015-07-28 18:58:31 +0200

    Quiz 7b: sequelize.js y SQLite

---
+++ models/quiz.js
@@ -0,0 +1,9 @@
+// Definición del modelo Quiz.
+
+module.exports = function (sequelize, DataType) {
+    return sequelize.define('Quiz',
+        {
+            pregunta: DataType.STRING,
+            respuesta: DataType.STRING
+        });
+}
